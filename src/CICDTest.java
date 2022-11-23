0ade649a28bcaa0ad23fc835b8e18f923c302cf1
dinithsithunaash <45732100+dinithsithunaash@users.noreply.github.com>
2022-11-23 10:17:16 +0530

    change in CICDTest.java

--- src/CICDTest.java
+++ src/CICDTest.java
@@ -4,5 +4,6 @@ public class CICDTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World Test");
+		System.out.println("2nd Test");
 		}
 }
